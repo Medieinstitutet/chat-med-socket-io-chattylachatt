be90a84a7c9fa65272442625c03c6f76141e5679
jess <jessica.response201@gmail.com>
2024-03-08 15:26:04 +0100

    basic setup, socket.io, sass, moment, ect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { ChangeEvent, useEffect, useState } from "react"; 
+import "./App.css";
+import './sass/style.scss';
+import { Socket, io } from "socket.io-client";
+import moment from 'moment-timezone';
+import 'moment/dist/locale/sv';
+import { Room } from "./models/Room";
+function App(){ 
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+  moment.tz.setDefault('Europe/Stockholm');
+  moment.locale('sv');
+  const [socket, setSocket] = useState<Socket>();
+  const [allRooms, setAllRooms] = useState<Room[]>([]);
+  const [selectedRoom, setSelectedRoom] = useState<Room>();
+  const [username, setUsername] = useState('')
+const [validUsername, setValidUsername] = useState<boolean>(false)
+
+
+
+
+  useEffect(() => {
+    if (socket) return;
+    const s = io("http://localhost:3000");
+    s.on("mainRoom", (mainRoomData: Room[]) => {
+      setSelectedRoom(mainRoomData[0]);
+    });
+    setSocket(s);
+  }, [setSocket, socket, ]);
+  const handleClick = (roomName: string) => {
+    socket?.emit("join_room", roomName, username, (data: Room) => {
+      setSelectedRoom(data);
+    });
+  };
+
+
+
+const checkIfUsernameValid =()=>{
+  /* Här behöver vi skapa logik för att se om användarnamnet är unikt */
+setValidUsername(true)
+socket?.emit("allroomsForUser", username,  (rooms: Room[]) => {
+  setAllRooms(rooms);
+}) 
+}
+
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+{!validUsername && <article>
+  <>
+  <label htmlFor='username'> Användarnamn: </label>
+<input id="username" type="text" value={username} 
+onChange={(e:ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+/> 
+<button onClick={checkIfUsernameValid}>Börja Chatta</button>
+  </>
+  </article>
 }
-
-export default App
+{validUsername && 
+<article> 
+  <h2>hello</h2>      
+<section>      
+{allRooms?.map((item) => (
+        <div key={item.id}  onClick={() => {handleClick(item.roomName);}} >
+        <p> {item.roomName} </p> 
+        </div>
+      ))}
+</section>
+    <h2>{selectedRoom?.roomName}</h2>
+      {selectedRoom?.messages.map((item) => (
+        <div key={item.cratedAt + item.user.username } >
+          <p> {item.cratedAt} </p> 
+        <p> {item.message} </p> 
+        <p> {item.user.username} </p> 
+        </div>
+      ))}
+</article>
+ }
+      </>
+  )
+   }
+export default App;
---
+++ server/src/index.ts
@@ -0,0 +1,141 @@
+
+import cors from "cors";
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import moment from "moment-timezone"
+import { Room } from "./models/Room";
+moment.tz.setDefault('Europe/Stockholm');
+  moment.locale('sv');
+
+let roomList: Room[] = [
+  {
+    id: '2024-03-07 15:12:04',
+    roomName: 'main',
+users:['loppan_123', 'grodanboll', 'pelle_kanin' ],
+
+    messages: [{
+      message: 'hej alla!',
+      cratedAt: '2024-03-05 12:14:04',
+      room: 'main', 
+      user:{username:'loppan_123', image:''}
+    },
+    {
+      message: 'hej loppan!',
+      cratedAt: '2024-03-05 15:12:04',
+      room: 'main', 
+   user:{ username:'grodanboll', image:''}
+    }]
+  },
+
+  {
+    id: '2024-03-07 15:12:05',
+    roomName: 'room 1',
+    users:['loppan_123', 'grodanboll', 'pelle_kanin'],
+
+    messages: [
+      {
+        message: 'hej hej',
+        cratedAt: '2024-03-07 15:12:04',
+        room: 'room 1', 
+     user:{username:'loppan_123', image:''}
+      },
+      
+      
+      {
+      message: 'hej loppan!',
+      cratedAt: '2024-03-07 15:15:00',
+      room: 'room 1', 
+   user:{username: 'pelle_kanin', image:''}
+    },
+  
+    {
+      message: 'hej alla!',
+      cratedAt: '2024-03-07 15:15:00',
+      room: 'room 1', 
+   user:{username: 'grodanboll', image:''}
+    }
+  
+  ]
+  }
+];
+
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+io.on("connection", (socket) => {  
+  console.log("a user connected");
+
+/* När användaren valt använtaren och kommer in på sidan så ges alla meddelanden som finns i main-room */
+  socket.emit(
+    "mainRoom",
+    roomList.map((item) => {
+        if (item.roomName === 'main'){
+           return {id: item.id, roomName: item.roomName, messages:item.messages}
+        }   
+
+    })
+    
+    );
+
+
+/* användare får all rum som den till hör skickat till sig */
+socket.on("allroomsForUser",(username, callback) => {
+  let roomUserList:Room[] =[]
+roomList.forEach(room =>{
+
+  /* Lägger till användaren i main om användaren inte redan finns */
+  roomList.map((item) => {
+    if (item.roomName === 'main'){
+       if(!item.users.includes(username)){
+        item.users.push(username)
+       }
+    }   
+
+})
+
+ const getroom = room.users.find((item) => item === username)
+
+if(getroom){
+ roomUserList.push({...room});
+}
+ })
+
+if(roomUserList){
+  callback(roomUserList); 
+}
+ 
+})   
+
+/* Användare ansluter till rum */
+socket.on("join_room", (roomName: string, username:string, callback) => {
+
+ 
+
+  socket.rooms.forEach((room) => {
+    console.log("Leaving room: ", room);
+    socket.leave(room);
+  });
+
+  socket.join(roomName);
+/* add new user to room */
+  const newUser  = roomList.map((list)  => { 
+    if(list.roomName === roomName && !list.users.includes(username)){
+    list.users.push(username)
+     } })
+
+  callback(roomList.find((rooms) => rooms.roomName === roomName))
+});
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
